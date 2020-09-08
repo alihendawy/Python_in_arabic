f8bbf9ea1a7893e58ff8865060865ea7421414a9
alihendawy <40849690+alihendawy@users.noreply.github.com>
2020-09-09 00:58:00 +0400

    Add files via upload

---
+++ lesson1 - strings.py
@@ -0,0 +1,78 @@
+#### Variables
+##x=12
+##y=4.3
+##z='label'
+
+#### Label for data that can be called at anytime
+#### Must be defined, 
+#### Can be overwritten
+
+#### Data types
+
+####Integers & floats
+
+##x=12
+##y=x+2 
+##z= y-1
+
+##x=2.2
+##y=x/2
+##z=y*3
+
+##x=2
+##y=x**2
+
+#### Strings
+
+##x='Ali'
+##y="Mohsen"
+##z='ali2016'
+
+####Indexable
+##print(x[0])
+##print(y[3])
+##print(z[-1])
+
+####concatenate
+##print(x+y)
+##print(x+' '+y)
+##print(x+" "+"999")
+##print(x+' '+y+' '+z)
+##print("Hello my name is "+x)
+
+####slicing
+##print(y[0:3])
+##print(z[1:4])
+##print(x[-1])
+##print(x[-2])
+##print(x[-3:-1])
+##print(y[:3])
+##print(z[3:])
+
+####Methods
+##print(y.upper())
+##print(y.lower())
+##print(z.capitalize())
+
+##a="ayham"
+##print(a.count('a'))
+##print(a.index('h'))
+##print(a.index('a'))
+
+##s=' king '
+##print(s)
+##print(s.strip())
+##print(s.rstrip())
+##print(s.lstrip())
+
+##txt="ARM-212"
+##print(txt.split("-"))
+##txt='arm-12-13'
+##print(txt.split("-"))
+
+####Immutable
+##txt[0]="B"
+
+
+## https://www.w3schools.com/python/python_ref_string.asp
+
