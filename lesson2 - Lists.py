d8236f48820af8abbf379368e004506d982de9d6
alihendawy <40849690+alihendawy@users.noreply.github.com>
2020-09-11 02:00:57 +0400

    lesson2

---
+++ lesson2 - Lists.py
@@ -0,0 +1,81 @@
+# Lists
+# x = [12, 25, 43, 16, 93]
+# print(x)
+#
+# print(x[0])
+# print(x[3])
+# print(x[-1])
+#
+# print(x[0:2])
+# print(x[1:4])
+#
+# print(x[:3])
+# print(x[2:])
+#
+# users = ["ali", "bushra", "khalid", "salwa"]
+# print(users)
+#
+# y = ["amr", 1, 56.4, "GG"]
+# print(y)
+#
+# users = ["ali", "bushra", "khalid", "salwa"]
+# account_id = [11, 12, 31, 54]
+# #
+# index = 2
+# print(users[index]+"'s account id is"+" "+str(account_id[index]))
+#
+# nested = [[1, 2, 3], [12, 13, 14], [56, 57, 58]]
+#
+# print(nested[1])
+# print(nested[1][2])
+#
+# # Methods
+# x = [5, 4, 7]
+# y = x+10
+# print(y)
+# x.append(10)
+# print(x)
+# x.insert(1, 12)
+# print(x)
+#
+# y = ["a", "c"]
+
+# x.extend(y)
+# print(x)
+# print(y)
+
+# y.extend(x)
+# print(y)
+# print(x)
+#
+# print(y.index("c"))
+# print(x.index(4))
+#
+# x = [12, 34, 1, 6, 43]
+# x.sort()
+# print(x)
+# x.reverse()
+# print(x)
+#
+# print(len(x))
+#
+# x = [1, 2, 3]
+# y = x
+# x[0] = 4
+# print(x)
+# print(y)
+#
+# y = x.copy()
+# x[0] = 6
+# print(x)
+# print(y)
+#
+# x.clear()
+# print(x)
+#
+# users = ["ali", "bushra", "khalid", "salwa"]
+# account_id = [11, 12, 31, 54]
+# i = users.index("khalid")
+# print(users[i])
+# print(account_id[i])
+# print(users[i]+"'s id numbers is "+str(account_id[i]))
